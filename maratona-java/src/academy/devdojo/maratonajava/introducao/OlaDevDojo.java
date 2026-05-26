83f6fcdf49c17df015d5a02838456808f4ac375a
Daniel Barbosa <tvdan2064@gmail.com>
2026-05-26 15:21:17 -0300

    up

--- maratona-java/src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -2,7 +2,7 @@ package academy.devdojo.maratonajava.introducao;
 
 public class OlaDevDojo {
     public static void main(String[] args){
-        System.out.println("KA-ME-HA-ME-HA teste");
+        System.out.println("KA-ME-HA-ME-HA");
     }
 }
 
