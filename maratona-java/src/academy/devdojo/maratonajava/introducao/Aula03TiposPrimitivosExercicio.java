9b00851e80c93b5c82e68c746c83c8193534c3c1
Daniel Barbosa <tvdan2064@gmail.com>
2026-05-27 17:34:49 -0300

    aprendendo jtipos primitivos etc..

---
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula03TiposPrimitivosExercicio {
+    public static void main(String[] args){
+        String nome = "Daniel";
+        String endereco = "Rua Maradona";
+        float salario = 2500;
+        String data = "25/10/2007";
+        System.out.println("Eu "+nome+", morando no endereço "+endereco+", confirmo que recebi o salário de "+salario+", na data "+data);
+    }
+
+
+
+}
